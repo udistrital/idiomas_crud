86ec83fd34c23a915098095b3f607d3c4c1f7f2e
Mafe Avendaño <mafe.ud@gmail.com>
2018-07-18 17:53:17 -0500

    Creación del API para el registro de los conocimientos en idiomas

---
+++ routers/router.go
@@ -0,0 +1,50 @@
+// @APIVersion 1.0.0
+// @Title beego Test API
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+	"github.com/udistrital/idiomas_crud/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+
+		beego.NSNamespace("/soporte_conocimiento_idioma",
+			beego.NSInclude(
+				&controllers.SoporteConocimientoIdiomaController{},
+			),
+		),
+
+		beego.NSNamespace("/idioma",
+			beego.NSInclude(
+				&controllers.IdiomaController{},
+			),
+		),
+
+		beego.NSNamespace("/clasificacion_nivel_idioma",
+			beego.NSInclude(
+				&controllers.ClasificacionNivelIdiomaController{},
+			),
+		),
+
+		beego.NSNamespace("/valor_nivel_idioma",
+			beego.NSInclude(
+				&controllers.ValorNivelIdiomaController{},
+			),
+		),
+
+		beego.NSNamespace("/conocimiento_idioma",
+			beego.NSInclude(
+				&controllers.ConocimientoIdiomaController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
