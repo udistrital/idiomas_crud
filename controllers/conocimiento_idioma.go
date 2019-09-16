e9e705b7aa30023c82386c4806a0fb43e8fa750b
AlaskaRising <nicostevrr@gmail.com>
2019-09-16 11:51:46 -0500

    :whale: Junto a @mariaalejandra9404 Se agregan archivos necesarios para despliegue en Test, adicionalmente se hace refactor #3

--- controllers/conocimiento_idioma.go
+++ controllers/conocimiento_idioma.go
@@ -3,11 +3,13 @@ package controllers
 import (
 	"encoding/json"
 	"errors"
-	"github.com/udistrital/idiomas_crud/models"
 	"strconv"
 	"strings"
 
+	"github.com/udistrital/idiomas_crud/models"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 )
 
 // ConocimientoIdiomaController operations for ConocimientoIdioma
@@ -29,7 +31,7 @@ func (c *ConocimientoIdiomaController) URLMapping() {
 // @Description create ConocimientoIdioma
 // @Param	body		body 	models.ConocimientoIdioma	true		"body for ConocimientoIdioma content"
 // @Success 201 {int} models.ConocimientoIdioma
-// @Failure 403 body is empty
+// @Failure 400 the request contains incorrect syntax
 // @router / [post]
 func (c *ConocimientoIdiomaController) Post() {
 	var v models.ConocimientoIdioma
@@ -38,10 +40,16 @@ func (c *ConocimientoIdiomaController) Post() {
 			c.Ctx.Output.SetStatus(201)
 			c.Data["json"] = v
 		} else {
-			c.Data["json"] = err.Error()
+			logs.Error(err)
+			//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+			c.Data["system"] = err
+			c.Abort("400")
 		}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("400")
 	}
 	c.ServeJSON()
 }
@@ -51,14 +59,17 @@ func (c *ConocimientoIdiomaController) Post() {
 // @Description get ConocimientoIdioma by id
 // @Param	id		path 	string	true		"The key for staticblock"
 // @Success 200 {object} models.ConocimientoIdioma
-// @Failure 403 :id is empty
+// @Failure 404 not found resource
 // @router /:id [get]
 func (c *ConocimientoIdiomaController) GetOne() {
 	idStr := c.Ctx.Input.Param(":id")
 	id, _ := strconv.Atoi(idStr)
 	v, err := models.GetConocimientoIdiomaById(id)
 	if err != nil {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("404")
 	} else {
 		c.Data["json"] = v
 	}
@@ -75,7 +86,7 @@ func (c *ConocimientoIdiomaController) GetOne() {
 // @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
 // @Param	offset	query	string	false	"Start position of result set. Must be an integer"
 // @Success 200 {object} models.ConocimientoIdioma
-// @Failure 403
+// @Failure 404 not found resource
 // @router / [get]
 func (c *ConocimientoIdiomaController) GetAll() {
 	var fields []string
@@ -121,8 +132,14 @@ func (c *ConocimientoIdiomaController) GetAll() {
 
 	l, err := models.GetAllConocimientoIdioma(query, fields, sortby, order, offset, limit)
 	if err != nil {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("404")
 	} else {
+		if l == nil {
+			l = append(l, map[string]interface{}{})
+		}
 		c.Data["json"] = l
 	}
 	c.ServeJSON()
@@ -134,7 +151,7 @@ func (c *ConocimientoIdiomaController) GetAll() {
 // @Param	id		path 	string	true		"The id you want to update"
 // @Param	body		body 	models.ConocimientoIdioma	true		"body for ConocimientoIdioma content"
 // @Success 200 {object} models.ConocimientoIdioma
-// @Failure 403 :id is not int
+// @Failure 400 the request contains incorrect syntax
 // @router /:id [put]
 func (c *ConocimientoIdiomaController) Put() {
 	idStr := c.Ctx.Input.Param(":id")
@@ -142,12 +159,18 @@ func (c *ConocimientoIdiomaController) Put() {
 	v := models.ConocimientoIdioma{Id: id}
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
 		if err := models.UpdateConocimientoIdiomaById(&v); err == nil {
-			c.Data["json"] = "OK"
+			c.Data["json"] = v
 		} else {
-			c.Data["json"] = err.Error()
+			logs.Error(err)
+			//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+			c.Data["system"] = err
+			c.Abort("400")
 		}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("400")
 	}
 	c.ServeJSON()
 }
@@ -157,15 +180,18 @@ func (c *ConocimientoIdiomaController) Put() {
 // @Description delete the ConocimientoIdioma
 // @Param	id		path 	string	true		"The id you want to delete"
 // @Success 200 {string} delete success!
-// @Failure 403 id is empty
+// @Failure 404 not found resource
 // @router /:id [delete]
 func (c *ConocimientoIdiomaController) Delete() {
 	idStr := c.Ctx.Input.Param(":id")
 	id, _ := strconv.Atoi(idStr)
 	if err := models.DeleteConocimientoIdioma(id); err == nil {
-		c.Data["json"] = "OK"
+		c.Data["json"] = map[string]interface{}{"Id": id}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("404")
 	}
 	c.ServeJSON()
 }
--- controllers/soporte_conocimiento_idioma.go
+++ controllers/soporte_conocimiento_idioma.go
@@ -3,11 +3,13 @@ package controllers
 import (
 	"encoding/json"
 	"errors"
-	"github.com/udistrital/idiomas_crud/models"
 	"strconv"
 	"strings"
 
+	"github.com/udistrital/idiomas_crud/models"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 )
 
 // SoporteConocimientoIdiomaController operations for SoporteConocimientoIdioma
@@ -29,7 +31,7 @@ func (c *SoporteConocimientoIdiomaController) URLMapping() {
 // @Description create SoporteConocimientoIdioma
 // @Param	body		body 	models.SoporteConocimientoIdioma	true		"body for SoporteConocimientoIdioma content"
 // @Success 201 {int} models.SoporteConocimientoIdioma
-// @Failure 403 body is empty
+// @Failure 400 the request contains incorrect syntax
 // @router / [post]
 func (c *SoporteConocimientoIdiomaController) Post() {
 	var v models.SoporteConocimientoIdioma
@@ -38,10 +40,16 @@ func (c *SoporteConocimientoIdiomaController) Post() {
 			c.Ctx.Output.SetStatus(201)
 			c.Data["json"] = v
 		} else {
-			c.Data["json"] = err.Error()
+			logs.Error(err)
+			//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+			c.Data["system"] = err
+			c.Abort("400")
 		}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("400")
 	}
 	c.ServeJSON()
 }
@@ -51,14 +59,17 @@ func (c *SoporteConocimientoIdiomaController) Post() {
 // @Description get SoporteConocimientoIdioma by id
 // @Param	id		path 	string	true		"The key for staticblock"
 // @Success 200 {object} models.SoporteConocimientoIdioma
-// @Failure 403 :id is empty
+// @Failure 404 not found resource
 // @router /:id [get]
 func (c *SoporteConocimientoIdiomaController) GetOne() {
 	idStr := c.Ctx.Input.Param(":id")
 	id, _ := strconv.Atoi(idStr)
 	v, err := models.GetSoporteConocimientoIdiomaById(id)
 	if err != nil {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("404")
 	} else {
 		c.Data["json"] = v
 	}
@@ -75,7 +86,7 @@ func (c *SoporteConocimientoIdiomaController) GetOne() {
 // @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
 // @Param	offset	query	string	false	"Start position of result set. Must be an integer"
 // @Success 200 {object} models.SoporteConocimientoIdioma
-// @Failure 403
+// @Failure 404 not found resource
 // @router / [get]
 func (c *SoporteConocimientoIdiomaController) GetAll() {
 	var fields []string
@@ -121,8 +132,14 @@ func (c *SoporteConocimientoIdiomaController) GetAll() {
 
 	l, err := models.GetAllSoporteConocimientoIdioma(query, fields, sortby, order, offset, limit)
 	if err != nil {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("404")
 	} else {
+		if l == nil {
+			l = append(l, map[string]interface{}{})
+		}
 		c.Data["json"] = l
 	}
 	c.ServeJSON()
@@ -134,7 +151,7 @@ func (c *SoporteConocimientoIdiomaController) GetAll() {
 // @Param	id		path 	string	true		"The id you want to update"
 // @Param	body		body 	models.SoporteConocimientoIdioma	true		"body for SoporteConocimientoIdioma content"
 // @Success 200 {object} models.SoporteConocimientoIdioma
-// @Failure 403 :id is not int
+// @Failure 400 the request contains incorrect syntax
 // @router /:id [put]
 func (c *SoporteConocimientoIdiomaController) Put() {
 	idStr := c.Ctx.Input.Param(":id")
@@ -142,12 +159,18 @@ func (c *SoporteConocimientoIdiomaController) Put() {
 	v := models.SoporteConocimientoIdioma{Id: id}
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
 		if err := models.UpdateSoporteConocimientoIdiomaById(&v); err == nil {
-			c.Data["json"] = "OK"
+			c.Data["json"] = v
 		} else {
-			c.Data["json"] = err.Error()
+			logs.Error(err)
+			//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+			c.Data["system"] = err
+			c.Abort("400")
 		}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("400")
 	}
 	c.ServeJSON()
 }
@@ -157,15 +180,18 @@ func (c *SoporteConocimientoIdiomaController) Put() {
 // @Description delete the SoporteConocimientoIdioma
 // @Param	id		path 	string	true		"The id you want to delete"
 // @Success 200 {string} delete success!
-// @Failure 403 id is empty
+// @Failure 404 not found resource
 // @router /:id [delete]
 func (c *SoporteConocimientoIdiomaController) Delete() {
 	idStr := c.Ctx.Input.Param(":id")
 	id, _ := strconv.Atoi(idStr)
 	if err := models.DeleteSoporteConocimientoIdioma(id); err == nil {
-		c.Data["json"] = "OK"
+		c.Data["json"] = map[string]interface{}{"Id": id}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("404")
 	}
 	c.ServeJSON()
 }
