43ed57c93fbff3ae8aed549a8e270b4b024765c8
AlaskaRising <nicostevrr@gmail.com>
2019-12-10 12:56:18 -0500

    :fireworks: Se corrige migracion.  #17

--- database/migrations/20191210_124030_insert_nivel.go
+++ database/migrations/20191210_124030_insert_nivel.go
@@ -20,12 +20,12 @@ func init() {
 // Run the migrations
 func (m *InsertNivel_20191210_124030) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
-	m.SQL("INSERT INTO idiomas.nivel(id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (1,'A1','Nivel A1','A1', true,1, LOCALTIMESTAMP, LOCALTIMESTAMP);")
-	m.SQL("INSERT INTO idiomas.nivel(id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (2,'A2','Nivel A2','A2', true,2, LOCALTIMESTAMP, LOCALTIMESTAMP);")
-	m.SQL("INSERT INTO idiomas.nivel(id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (3,'B1','Nivel B1','B1', true,3, LOCALTIMESTAMP, LOCALTIMESTAMP);")
-	m.SQL("INSERT INTO idiomas.nivel(id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (4,'B2','Nivel B2','B2', true,4, LOCALTIMESTAMP, LOCALTIMESTAMP);")
-	m.SQL("INSERT INTO idiomas.nivel(id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (5,'C1','Nivel C1','C1', true,5, LOCALTIMESTAMP, LOCALTIMESTAMP);")
-	m.SQL("INSERT INTO idiomas.nivel(id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (6,'C2','Nivel C2','C2', true,6, LOCALTIMESTAMP, LOCALTIMESTAMP);")
+	m.SQL("INSERT INTO idiomas.nivel (id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (1,'A1','Nivel A1','A1', true,1, LOCALTIMESTAMP, LOCALTIMESTAMP);")
+	m.SQL("INSERT INTO idiomas.nivel (id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (2,'A2','Nivel A2','A2', true,2, LOCALTIMESTAMP, LOCALTIMESTAMP);")
+	m.SQL("INSERT INTO idiomas.nivel (id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (3,'B1','Nivel B1','B1', true,3, LOCALTIMESTAMP, LOCALTIMESTAMP);")
+	m.SQL("INSERT INTO idiomas.nivel (id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (4,'B2','Nivel B2','B2', true,4, LOCALTIMESTAMP, LOCALTIMESTAMP);")
+	m.SQL("INSERT INTO idiomas.nivel (id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (5,'C1','Nivel C1','C1', true,5, LOCALTIMESTAMP, LOCALTIMESTAMP);")
+	m.SQL("INSERT INTO idiomas.nivel (id,nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) VALUES (6,'C2','Nivel C2','C2', true,6, LOCALTIMESTAMP, LOCALTIMESTAMP);")
 	
 }
 
